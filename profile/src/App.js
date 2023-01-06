3062a028de893d7a6e083feb8e10318f21bc514d
Jerrysun817 <sunyiran0817@gmail.com>
2023-01-06 14:35:28 -0800

    update footer

--- profile/src/App.js
+++ profile/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Header from './Header';
 import Description from './Description';
 import SubSection from './SubSection';
+import Footer from './footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Description />
+      <h1>Experiences</h1>
       <SubSection />
+      <Footer />
     </div>
   );
 }
